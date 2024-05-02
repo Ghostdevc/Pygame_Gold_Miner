9a28a68e08d7730733e89b9f5a6ab118b671ff16
Ghost <b.basturk.bb@gmail.com>
2024-05-03 01:53:21 +0300

    Game Skeleton Created

--- Gold_Miner/main.py
+++ Gold_Miner/main.py
@@ -1,3 +1,17 @@
 import pygame
 
 pygame.init()
+
+#VARIABLE DEFINITIONS
+
+
+#GAME LOOP
+run = True
+
+while run:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
